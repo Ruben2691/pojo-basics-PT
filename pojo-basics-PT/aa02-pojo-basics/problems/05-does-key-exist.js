0c77ea9b3b14936cd8a3c2a893058d808d3b0ee0
Ruben Cuello <kuelmusic@gmail.com>
2024-03-28 20:26:32 -0400

    05

--- pojo-basics-PT/aa02-pojo-basics/problems/05-does-key-exist.js
+++ pojo-basics-PT/aa02-pojo-basics/problems/05-does-key-exist.js
@@ -5,14 +5,19 @@ key is not inside the object.
 
 Examples:
 
-let obj1 = {bootcamp: 'App Academy', course: 'Bootcamp Prep'}
-doesKeyExist(obj1, 'course'); // => true
-doesKeyExist(obj1, 'name'); // => false
+
 ***********************************************************************/
 
 function doesKeyExist(obj, key) {
-  // Your code here 
+  if(obj[key] !== undefined){
+    return true
+  }else{
+    return false
+  }
 }
 
+let obj1 = {bootcamp: 'App Academy', course: 'Bootcamp Prep'}
+doesKeyExist(obj1, 'course'); // => true
+doesKeyExist(obj1, 'name'); // => false
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = doesKeyExist;
